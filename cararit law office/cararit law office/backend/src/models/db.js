5b002e3af4a3594b80762421890d00514279ca88
gelmarcute <gelmargazzingan69@gmail.com>
2026-05-09 16:37:46 +0800

    Fixed CORS error and updated login URL

--- cararit law office/cararit law office/backend/src/models/db.js
+++ cararit law office/cararit law office/backend/src/models/db.js
@@ -1,38 +1,26 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
 const db = mysql.createPool({
-
   host: process.env.MYSQLHOST,
-
   user: process.env.MYSQLUSER,
-
   password: process.env.MYSQLPASSWORD,
-
   database: process.env.MYSQLDATABASE,
-
   port: process.env.MYSQLPORT,
-
   waitForConnections: true,
-
   connectionLimit: 10,
-
-  queueLimit: 0
+  queueLimit: 0,
+  dateStrings: true
 });
 
+// I-test ang connection kapag tinawag itong file
 db.getConnection((err, connection) => {
-
   if (err) {
-
-    console.log("❌ DATABASE CONNECTION ERROR");
-
-    console.log(err);
-
-    return;
+    console.error('❌ Models/DB MySQL Connection Error:', err.message);
+  } else {
+    console.log('✅ Models/DB Successfully Connected to MySQL Database');
+    connection.release();
   }
-
-  console.log("✅ MYSQL CONNECTED");
-
-  connection.release();
 });
 
 module.exports = db;

4537b31bfa1816ffba7b19a65acf21064932f676
gelmarcute <gelmargazzingan69@gmail.com>
2026-05-09 16:55:22 +0800

    Fixed CORS error and updated login URL

--- cararit law office/cararit law office/backend/src/index.js
+++ cararit law office/cararit law office/backend/src/index.js
@@ -44,7 +44,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Sagutin ang lahat ng OPTIONS (Preflight) requests
-app.options('*', cors(corsOptions));
+app.options('/*', cors(corsOptions));
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
